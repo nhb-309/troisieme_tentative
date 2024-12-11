cbf47b20ff8e7c943ffc04e1889adebdc0df669b
numa <nhbosc@gmail.com>
2024-12-11 09:55:32 +0100

    aaa

--- exos_fiche2.R
+++ exos_fiche2.R
@@ -29,6 +29,22 @@ rg_joueurs2=rg %>%
   pivot_longer(!Result,names_to = 'players') %>% 
   group_by(value) %>% 
   summarise(total_victoires=sum(Result))
+Joueur = 'Roger Federer'
+
+victoires = function(Joueur){
+  return(
+  rg %>% 
+    filter(
+      (Player1 == Joueur & Result == 1) |
+        (Player2 == Joueur & Result == 0)) %>% 
+    nrow())
+  }
+  
+victoires(Joueur)
+x=rg %>% 
+  rowwise() %>% 
+  mutate(victoires = victoires(Player1))
+
 
 
 list.files()
@@ -39,6 +55,7 @@ oa=read_csv('openaustralie2013.csv') %>%
 tennis=bind_rows(list(oa=oa,rg=rg),.id='id') %>% 
   as_tibble()
 
+
 tennis %>% 
   group_by(id) %>% 
   summarise(n_match=n())
@@ -47,8 +64,52 @@ tennis %>%
   group_by(id,Round) %>% 
   mutate(total_aces = ACE.1+ACE.2) %>% 
   summarise(avg_aces=mean(total_aces,na.rm=T)) %>%  
+  summarise(avg_aces=mean(total_aces,na.rm=T)) %>% 
   pivot_wider(names_from = c(id),
               values_from = avg_aces)
+
+oa_joueurs2=tennis %>% 
+  select(id,Player1,Player2,Result) %>% 
+  pivot_longer(!c(id,Result)) %>% 
+  group_by(id,value) %>% 
+  summarise(total_win_oa = sum(Result)) %>% 
+  filter(id=='oa') %>% 
+  ungroup() %>% 
+  select(value,total_win_oa)
+
+rg_joueurs2 
+oa_joueurs2 
+
+rg_joueurs2 %>% 
+  left_join(oa_joueurs2,by='value')
+rg_joueurs2 %>% 
+  inner_join(oa_joueurs2,by='value')
+rg_joueurs2 %>% 
+  full_join(oa_joueurs2,by='value')
+
+
+
+
+rg_victoires=function(joueur){
+  return(
+    rg %>% filter(
+      (Player1 == joueur &  Result == 1) |
+        (Player2 == joueur & Result == 0)) %>% 
+      nrow()
+    
+  )
+}
+
+rg_victoires("Rafael Nadal")
+
+rg_joueurs
+colnames(rg_joueurs)=c('Joueur','total_rg_win')
+
+
+rg_joueurs %>% 
+  rowwise() %>% 
+  mutate(Victoires=rg_victoires(Joueur))
+
 
 
 
